3a6d85aa7b70d0d6bd5411fe9b074c640b50b8dc
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2023-12-10 15:53:59 +0600

    Update DispatchedServlet.java

--- demo2/src/main/java/com/controller/DispatchedServlet.java
+++ demo2/src/main/java/com/controller/DispatchedServlet.java
@@ -4,28 +4,32 @@ import com.dao.*;
 import java.io.*;
 import java.sql.*;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 
 //@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class DispatchedServlet extends HttpServlet{
 
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//        resp.setContentType("text/html");
-//        PrintWriter out = resp.getWriter();
-//
-//        String name = req.getParameter("user_name");
-//        String course = req.getParameter("user_course");
-//        String email = req.getParameter("user_email");
-//        String age = req.getParameter("user_age");
-//
-//        //MySql ob = new MySql();
-//        //ob.getData(name, email, course, age);
-//
-//        out.println("<html><body>");
-//        out.println("<h1>"+name+"</h1>");
-//        out.println("<h1>"+course+"</h1>");
-//        out.println("<h1>"+email+"</h1>");
-//        out.println("<h1>"+age+"</h1>");
-//        out.println("Inserted Successfully</body></html>");
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException , ServletException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        String name = req.getParameter("user_name");
+        String course = req.getParameter("user_course");
+        String email = req.getParameter("user_email");
+        String age = req.getParameter("user_age");
+
+        MySql ob = new MySql(name, email, course, age);
+
+        out.println("<html><body>");
+        out.println("<h1>"+name+"</h1>");
+        out.println("<h1>"+course+"</h1>");
+        out.println("<h1>"+email+"</h1>");
+        out.println("<h1>"+age+"</h1>");
+        out.println("Inserted Successfully</body></html>");
+
+        RequestDispatcher rd = req.getRequestDispatcher("form.jsp");
+        rd.include(req, resp);
     }
 }
